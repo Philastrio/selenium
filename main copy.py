718d95aa5fe1c6879ffa1f08e0569a405226e8f5
philastrio <sideproject0214@gmail.com>
2020-08-30 03:52:44 +0900

    selenium test

---
+++ main copy.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+""" KEYWORD = "buy domain"
+
+browser = webdriver.Chrome(ChromeDriverManager().install())
+browser.get("https://google.com")
+
+
+
+
+
+
+
+
+
+    class_name = search_result.get_attribute("class")
+    print(class_name)
+    # if "liYKde" not in class_name:
+    if "kno-kp mnr-c g-blk" not in class_name:
+        
+    # kp-wholepage kp-wholepage-osrp HSryR EyBRub
+
+
+shitty_element.screenshot("shitty.png")
+print(shitty_element)
+browser.close()
+
+ title = search_result.find_element_by_tag_name("h3")
+    if title:
+        print(title.text) 
+ """
